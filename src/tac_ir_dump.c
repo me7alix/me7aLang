3ae50372470cf80f45b3b6fc392383c9c96af194
me7alix <me7alix@gmail.com>
2025-12-03 00:29:35 +0300

    fix(tac_ir_dump): make better tac_ir_dump

--- src/tac_ir_dump.c
+++ src/tac_ir_dump.c
@@ -5,34 +5,72 @@
 
 #include "../include/tac_ir.h"
 
+char *tac_ir_dump_opr_type(TAC_Operand op) {
+	switch (tac_ir_get_opr_type(op).kind) {
+		case TYPE_INT:    return "int";
+		case TYPE_UINT:   return "uint";
+		case TYPE_I32:    return "i32";
+		case TYPE_I64:    return "i64";
+		case TYPE_U64:    return "u64";
+		case TYPE_I16:    return "i16";
+		case TYPE_U16:    return "u16";
+		case TYPE_I8:     return "i8";
+		case TYPE_U8:     return "u8";
+		case TYPE_BOOL:   return "bool";
+		case TYPE_UPTR:   return "uptr";
+		case TYPE_IPTR:   return "iptr";
+		case TYPE_POINTER:
+		case TYPE_ARRAY:  return "ptr";
+		case TYPE_STRUCT: return "struct";
+		default:          return "ERR";
+	}
+}
+
 void tac_ir_dump_opr(TAC_Operand opr, char *buf) {
 	switch (opr.kind) {
 		case OPR_NULL:     sprintf(buf, "NULL"); break;
-		case OPR_SIZEOF:   sprintf(buf, "sizeof:%d", opr.size_of.v_type.kind); break;
+		case OPR_SIZEOF:   sprintf(buf, "sizeof:%s", tac_ir_dump_opr_type(opr)); break;
 		case OPR_FUNC_INP: sprintf(buf, "FI(%u):%d", opr.func_inp.arg_id, opr.func_inp.type.kind); break;
-		case OPR_FUNC_RET: sprintf(buf, "FR:%d", opr.func_ret.type.kind); break;
+		case OPR_FUNC_RET: sprintf(buf, "FR:%s", tac_ir_dump_opr_type(opr)); break;
 		case OPR_NAME:     sprintf(buf, "\"%s\"", opr.name); break;
 		case OPR_LABEL:    sprintf(buf, ".L%u", opr.label_id);  break;
 		case OPR_FIELD:    sprintf(buf, ">%s", opr.field_id);  break;
 
 		case OPR_VAR: {
 			switch (opr.var.kind) {
-				case VAR_STACK:   sprintf(buf, "(%u):%d", opr.var.addr_id, opr.var.type.kind); break;
-				case VAR_ADDR:    sprintf(buf, "[%u]:%d", opr.var.addr_id, opr.var.type.kind); break;
-				case VAR_DATAOFF: sprintf(buf, "{%u}:%d", opr.var.addr_id, opr.var.type.kind); break;
+				case VAR_STACK:
+					sprintf(buf, "(%u):%s", opr.var.addr_id, tac_ir_dump_opr_type(opr));
+					break;
+				case VAR_ADDR:
+					sprintf(buf, "[%u]:%s", opr.var.addr_id, tac_ir_dump_opr_type(opr));
+					break;
+				case VAR_DATA:
+					sprintf(buf, "{%u}:%s", opr.var.addr_id, tac_ir_dump_opr_type(opr));
+					break;
 			}
 		} break;
 
 		case OPR_LITERAL: {
 			switch (opr.literal.type.kind) {
-				case TYPE_INT:   sprintf(buf, "%d:%d", (i32) opr.literal.lint, opr.literal.type.kind); break;
+				case TYPE_I32:
+				case TYPE_INT:
+					sprintf(buf, "%d:%s", (i32) opr.literal.lint, tac_ir_dump_opr_type(opr));
+					break;
 				case TYPE_I8:
-				case TYPE_BOOL:  sprintf(buf, "%d:%d", (i8) opr.literal.lint, opr.literal.type.kind); break;
+				case TYPE_BOOL:
+					sprintf(buf, "%d:%s", (i8) opr.literal.lint, tac_ir_dump_opr_type(opr));
+					break;
 				case TYPE_POINTER:
+				case TYPE_UPTR:
 				case TYPE_IPTR:
-				case TYPE_I64:   sprintf(buf, "%lli:%d", opr.literal.lint, opr.literal.type.kind); break;
-				case TYPE_F32:   sprintf(buf, "%f:%d", (float) opr.literal.lfloat, opr.literal.type.kind); break;
-				default: sprintf(buf, "ERR\n"); break;
+				case TYPE_I64:
+					sprintf(buf, "%lli:%s", opr.literal.lint, tac_ir_dump_opr_type(opr));
+					break;
+				case TYPE_F32:
+					sprintf(buf, "%f:%s", (float) opr.literal.lfloat, tac_ir_dump_opr_type(opr));
+					break;
+				default: sprintf(buf, "ERR");
+					break;
 			}
 		} break;
 	}
@@ -46,30 +84,30 @@ void tac_ir_dump_inst(TAC_Instruction inst, char *res) {
 	tac_ir_dump_opr(inst.dst, dst);
 
 	switch (inst.op) {
-		case OP_ADD:    sprintf(res, "    var%s = %s %s %s", dst, arg1, "+", arg2);  break;
-		case OP_EQ:     sprintf(res, "    var%s = %s %s %s", dst, arg1, "==", arg2); break;
-		case OP_NOT:    sprintf(res, "    var%s = !%s", dst, arg1); break;
-		case OP_OR:     sprintf(res, "    var%s = %s %s %s", dst, arg1, "||", arg2); break;
-		case OP_AND:    sprintf(res, "    var%s = %s %s %s", dst, arg1, "&&", arg2); break;
-		case OP_GREAT:  sprintf(res, "    var%s = %s %s %s", dst, arg1, ">", arg2);  break;
-		case OP_LESS:   sprintf(res, "    var%s = %s %s %s", dst, arg1, "<", arg2);  break;
-		case OP_GREAT_EQ:sprintf(res, "    var%s = %s %s %s", dst, arg1, ">=", arg2);  break;
-		case OP_LESS_EQ:sprintf(res, "    var%s = %s %s %s", dst, arg1, "<=", arg2);  break;
-		case OP_NOT_EQ: sprintf(res, "    var%s = %s %s %s", dst, arg1, "!=", arg2); break;
-		case OP_JUMP_IF_NOT: sprintf(res, "    jmp_if_not %s %s", dst, arg1); break;
-		case OP_JUMP:   sprintf(res, "    jmp %s", dst); break;
-		case OP_CAST:   sprintf(res, "    var%s = cast %s", dst, arg1); break;
-		case OP_NEG:    sprintf(res, "    var%s = -%s", dst, arg1); break;
-		case OP_LABEL:  sprintf(res, "%s", arg1); break;
-		case OP_SUB:    sprintf(res, "    var%s = %s %s %s", dst, arg1, "-", arg2);  break;
-		case OP_MUL:    sprintf(res, "    var%s = %s %s %s", dst, arg1, "*", arg2);  break;
-		case OP_DEREF:  sprintf(res, "    var%s = deref %s", dst, arg1);  break;
-		case OP_REF:    sprintf(res, "    var%s = ref %s", dst, arg1);  break;
-		case OP_DIV:    sprintf(res, "    var%s = %s %s %s", dst, arg1, "/", arg2);  break;
-		case OP_MOD:    sprintf(res, "    var%s = %s %s %s", dst, arg1, "%", arg2);  break;
-		case OP_ASSIGN: sprintf(res, "    var%s = %s", dst, arg1); break;
-		case OP_RETURN: sprintf(res, "    return %s", arg1); break;
-		case OP_FADDR:  sprintf(res, "    var%s = faddr %s %s", dst, arg1, arg2); break;
+		case OP_ADD:         sprintf(res, "    var%s = %s %s %s", dst, arg1, "+",  arg2);  break;
+		case OP_EQ:          sprintf(res, "    var%s = %s %s %s", dst, arg1, "==", arg2);  break;
+		case OP_NOT:         sprintf(res, "    var%s = !%s", dst, arg1);                   break;
+		case OP_OR:          sprintf(res, "    var%s = %s %s %s", dst, arg1, "||", arg2);  break;
+		case OP_AND:         sprintf(res, "    var%s = %s %s %s", dst, arg1, "&&", arg2);  break;
+		case OP_GREAT:       sprintf(res, "    var%s = %s %s %s", dst, arg1, ">",  arg2);  break;
+		case OP_LESS:        sprintf(res, "    var%s = %s %s %s", dst, arg1, "<",  arg2);  break;
+		case OP_GREAT_EQ:    sprintf(res, "    var%s = %s %s %s", dst, arg1, ">=", arg2);  break;
+		case OP_LESS_EQ:     sprintf(res, "    var%s = %s %s %s", dst, arg1, "<=", arg2);  break;
+		case OP_NOT_EQ:      sprintf(res, "    var%s = %s %s %s", dst, arg1, "!=", arg2);  break;
+		case OP_JUMP_IF_NOT: sprintf(res, "    jmp_if_not %s %s", dst, arg1);              break;
+		case OP_JUMP:        sprintf(res, "    jmp %s", dst);                              break;
+		case OP_CAST:        sprintf(res, "    var%s = cast %s", dst, arg1);               break;
+		case OP_NEG:         sprintf(res, "    var%s = -%s", dst, arg1);                   break;
+		case OP_LABEL:       sprintf(res, "%s", arg1);                                     break;
+		case OP_SUB:         sprintf(res, "    var%s = %s %s %s", dst, arg1, "-", arg2);   break;
+		case OP_MUL:         sprintf(res, "    var%s = %s %s %s", dst, arg1, "*", arg2);   break;
+		case OP_DEREF:       sprintf(res, "    var%s = deref %s", dst, arg1);              break;
+		case OP_REF:         sprintf(res, "    var%s = ref %s", dst, arg1);                break;
+		case OP_DIV:         sprintf(res, "    var%s = %s %s %s", dst, arg1, "/", arg2);   break;
+		case OP_MOD:         sprintf(res, "    var%s = %s %s %s", dst, arg1, "%", arg2);   break;
+		case OP_ASSIGN:      sprintf(res, "    var%s = %s", dst, arg1);                    break;
+		case OP_RETURN:      sprintf(res, "    return %s", arg1);                          break;
+		case OP_FADDR:       sprintf(res, "    var%s = faddr %s %s", dst, arg1, arg2);     break;
 		case OP_FUNC_CALL: {
 			char buf[128];
 			tac_ir_dump_opr(inst.dst, dst);
