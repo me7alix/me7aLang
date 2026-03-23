634a8a532b04b67cd67a0fa5461005a5d6f950a0
me7alix <me7alix@gmail.com>
2026-03-23 09:37:37 +0300

    refactor

--- include/preprocessor.h
+++ include/preprocessor.h
@@ -4,9 +4,33 @@
 #include "lexer.h"
 #include <stdbool.h>
 
-typedef DA(char*) Imports;
+typedef DA(Token) Tokens;
 
 typedef struct {
+	enum {
+		MACRO_OBJ,
+		MACRO_FUNC,
+	} kind;
+
+	union {
+		struct {
+			DA(char*) args;
+			Tokens body;
+		} func;
+
+		struct {
+			Tokens body;
+		} obj;
+	} as;
+} Macro;
+
+typedef DA(char*) Imports;
+HT_DECL(ImportedTable, char*, bool)
+HT_DECL_STR(MacroTable, Macro)
+
+typedef struct {
+	ImportedTable it;
+	MacroTable mt;
 	Imports *imports;
 	Lexer lexer;
 	size_t cur_tok;	
